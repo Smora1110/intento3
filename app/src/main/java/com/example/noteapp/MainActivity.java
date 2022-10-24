9afc7447b32035352c8613023faae80b33f48113
sebas <smora1110@gmail.com>
2022-10-23 20:04:05 -0500

    ag layouts

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_login);
 
         et_user = findViewById(R.id.et_user);
         et_contraseña = findViewById(R.id.et_contraseña);
